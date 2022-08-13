02627c4716ae95a8767d7b3b68af15e6f2e04cb7
Branson Morgan <bransox@comcast.net>
2022-08-13 17:55:25 -0400

    commit tree 7495983b3ae22fab2e84198885932ad1e381224c

--- main.js
+++ main.js
@@ -3,8 +3,40 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', () => {
+  const likeButton = document.querySelector('.like-glyph');
+  const modal = document.getElementById('modal');
 
-
+  function showError(cb) {
+    modal.removeAttribute('class');
+    setTimeout(function() {
+      modal.setAttribute('class', 'hidden');
+      cb();
+    }, 3000);
+  };
+  function likeMe(button) {
+    button.addEventListener('click', (e) => {
+      if (e.target.innerHTML === EMPTY_HEART) {
+        mimicServerCall()
+        .then(() => {e.target.innerHTML = FULL_HEART;
+        e.target.setAttribute('class', 'like-glyph activated-heart');
+      })
+      .catch(() => {
+        showError(function() {
+          e.target.innerHTML = FULL_HEART;
+          e.target.setAttribute('class', 'like-glyph activated-heart');
+        });
+      });
+      } else {
+        e.target.innerHTML = EMPTY_HEART
+        e.target.setAttribute('class', 'like-glyph')
+      }
+    });
+  };
+  likeButton.forEach(button => {
+    likeMe(button)
+  });
+});
 
 
 //------------------------------------------------------------------------------
